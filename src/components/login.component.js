e51c8b20763b57218799ecb7bb9303e4e43a4416
gkueny <gaetankueny@gmail.com>
2017-05-05 19:55:00 +0200

    add login component

---
+++ src/components/login.component.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+import '../assets/css/App.css';
+
+class LoginComponent extends Component {
+  render() {
+    return (
+      <div>
+        <p className="home-block">
+          LOGIN
+        </p>
+      </div>
+    );
+  }
+}
+
+export default LoginComponent;
