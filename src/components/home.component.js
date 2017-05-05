8e18fb98d425b55f6ef5a5869f1b482619aef49e
gkueny <gaetankueny@gmail.com>
2017-05-05 19:45:41 +0200

    add home component

---
+++ src/components/home.component.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+import '../assets/css/App.css';
+
+class HomeComponent extends Component {
+  render() {
+    return (
+      <div>
+        <p className="home-block">
+          HOME
+        </p>
+      </div>
+    );
+  }
+}
+
+export default HomeComponent;
