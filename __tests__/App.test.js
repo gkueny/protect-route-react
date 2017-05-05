5e1ae3567f083c6bc5c545fcd303489e4fefcd22
gkueny <gaetankueny@gmail.com>
2017-05-05 19:47:27 +0200

    change file name

--- __tests__/App.test.js
+++ __tests__/App.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import App from '../src/components/App';
+import AppComponent from '../src/components/app.component';
 
 it('App component render without crashing', () => {
-  shallow(<App />);
+  shallow(<AppComponent />);
 });
