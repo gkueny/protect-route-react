62ca43fa3070cdcd524921a850dc23bf046807b2
gkueny <gaetankueny@gmail.com>
2017-05-05 19:50:06 +0200

    add admin route

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import AppComponent from './components/app.component';
 import HomeComponent from './components/home.component';
+import AdminComponent from './components/admin.component';
 
 import './assets/css/index.css';
 
@@ -12,6 +13,7 @@ ReactDOM.render(
   <Router>
     <AppComponent>
       <Route exact path="/" component={HomeComponent} />
+      <Route path="/admin" component={AdminComponent} />
     </AppComponent>
   </Router>,
   document.getElementById('root'),
