4c1f89597cfc085cb2aed36f1c769c72b8f09f8c
gkueny <gaetankueny@gmail.com>
2017-05-08 14:35:06 +0200

    update login test to initialize firebase before test component

--- __tests__/login.test.js
+++ __tests__/login.test.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import LoginComponent from '../src/components/login.component';
+import * as firebase from 'firebase';
+import { config } from '../src/vars/firebase.config';
 
 it('Login component render without crashing', () => {
+  firebase.initializeApp(config);
+
   shallow(<LoginComponent />);
 });
